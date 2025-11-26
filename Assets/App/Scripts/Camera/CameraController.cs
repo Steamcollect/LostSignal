f3d3dedd54e60dfa5fc7345359729f45bf64d973
Steamcollect <eliot.garnier.080805@gmail.com>
2025-11-26 20:37:17 +0100

    Explorative camera

--- Assets/App/Scripts/Camera/CameraController.cs
+++ Assets/App/Scripts/Camera/CameraController.cs
@@ -1,5 +1,6 @@
-using MVsToolkit.Dev;
+﻿using MVsToolkit.Dev;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
@@ -10,6 +11,10 @@ public class CameraController : MonoBehaviour
 
     Vector3 velocity;
 
+    [Space(10)]
+    [SerializeField] float maxExplorationDistance = 5;
+    [SerializeField] AnimationCurve multPerDist;
+
     [Header("References")]
     [SerializeField] Camera cam;
     
@@ -17,7 +22,8 @@ public class CameraController : MonoBehaviour
 
     [Header("Input")]
     [SerializeField] RSE_SetCameraTarget setTarget;
-    
+    [SerializeField] InputActionReference mousePosIA;
+
     [Header("Output")]
     [SerializeField] RSO_MainCamera rso_MainCamera;
 
@@ -44,7 +50,46 @@ public class CameraController : MonoBehaviour
     {
         if (target == null) return;
 
-        transform.position = Vector3.SmoothDamp(transform.position, target.position + posOffset, ref velocity, timeOffset);
+        Vector3 basePos = target.position + posOffset;
+        Vector3 mousePos = GetMouseWorldPos();
+
+        // Direction depuis le joueur → souris
+        Vector3 dir = (mousePos - target.position).normalized;
+
+        // Distance normalisée 0–1
+        float dist01 = Mathf.Clamp(
+            Vector3.Distance(target.position, mousePos),
+            0, maxExplorationDistance
+        ) / maxExplorationDistance;
+
+        // Atténuation via ton AnimationCurve
+        float mult = multPerDist.Evaluate(dist01);
+
+        // Décalage final d’exploration
+        Vector3 explorationOffset = dir * maxExplorationDistance * mult;
+
+        // Position finale (comme Death’s Door)
+        Vector3 finalPos = basePos + explorationOffset;
+
+        // Smooth
+        transform.position = Vector3.SmoothDamp(
+            transform.position,
+            finalPos,
+            ref velocity,
+            timeOffset
+        );
+    }
+
+    Vector3 GetMouseWorldPos()
+    {
+        Ray ray = cam.ScreenPointToRay(mousePosIA .action.ReadValue<Vector2>());
+
+        if (Mathf.Abs(ray.direction.y) < 0.0001f)
+            return Vector3.zero;
+
+        float t = (target.position.y - ray.origin.y) / ray.direction.y;
+
+        return ray.origin + ray.direction * t;
     }
 
     void SetTarget(Transform target) { this.target = target; }
