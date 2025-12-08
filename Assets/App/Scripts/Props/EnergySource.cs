1823c7409bd9dacc33d302e746e3ea3e8075cbc2
MrSoubi <emm.bouis@gmail.com>
2025-12-08 13:34:18 +0100

    Ajout indicateur

--- Assets/App/Scripts/Props/EnergySource.cs
+++ Assets/App/Scripts/Props/EnergySource.cs
@@ -7,12 +7,34 @@ public class EnergySource : MonoBehaviour
     [SerializeField] private float maxHealth = 100f;
     private float currentHealth;
     [SerializeField] private List<EntityHealth> linkedObjects;
+    private List<LineRenderer> lineRenderers = new List<LineRenderer>();
+    public Material lineMaterial;
     void Start()
     {
         currentHealth = maxHealth;
         foreach (EntityHealth linkedObject in linkedObjects)
         {
             linkedObject.GainInvincibility();
+            LineRenderer lr = gameObject.AddComponent<LineRenderer>();
+            lineRenderers.Add(lr);
+            lr.positionCount = 2;
+            lr.startWidth = 0.1f;
+            lr.endWidth = 0.1f; 
+            lr.material = lineMaterial;
+            lr.SetPosition(0, transform.position);
+            lr.SetPosition(1, linkedObject.transform.position);
+        }
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < linkedObjects.Count; i++)
+        {
+            if (linkedObjects[i] != null)
+            {
+                lineRenderers[i].SetPosition(0, transform.position);
+                lineRenderers[i].SetPosition(1, linkedObjects[i].transform.position);
+            }
         }
     }
 
@@ -29,8 +51,6 @@ public class EnergySource : MonoBehaviour
             {
                 TakeDamage(grenade.Damage);
             }
-            
-            
         }
     }
     
