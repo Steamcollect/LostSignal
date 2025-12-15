177a604735eb23dc9dcca9bac2877f337b258d3c
Steamcollect <eliot.garnier.080805@gmail.com>
2025-12-15 13:49:08 +0100

    Add tool for cools visual

--- Assets/App/Scripts/CombatStyle/RangeOverload_CombatStyle.cs
+++ Assets/App/Scripts/CombatStyle/RangeOverload_CombatStyle.cs
@@ -6,17 +6,16 @@ using UnityEngine.InputSystem;
 
 public class RangeOverload_CombatStyle : CombatStyle
 {
-    [Header("RANGE OVERLOAD")]
     [SerializeField, MVsToolkit.Dev.ReadOnly] float m_CurentTemperature;
-    [SerializeField, MVsToolkit.Dev.ReadOnly] WeaponState m_CurrentState;
-    enum WeaponState
+    [SerializeField, MVsToolkit.Dev.ReadOnly] RangeOverloadWeaponState m_CurrentState;
+    public enum RangeOverloadWeaponState
     {
-        CanShoot,
-        Overload,
-        DefaultCool,
-        OverloadCool,
-        CoolBuffed,
-        CoolNerfed
+        CanShoot = 0,
+        Overload = 1,
+        DefaultCool = 2,
+        OverloadCool = 3,
+        CoolBuffed = 4,
+        CoolNerfed = 5
     }
 
     [Space(10)]
@@ -37,17 +36,17 @@ public class RangeOverload_CombatStyle : CombatStyle
     [SerializeField] Vector2 m_RangeToNerf;
 
     [Space(10)]
-    [FoldoutGroup("Visual"), SerializeField] MeshRenderer m_MeshRenderer;
-    [FoldoutGroup("Visual"), SerializeField] Gradient m_ColorOverTemperature;
+    [SerializeField] MeshRenderer m_MeshRenderer;
+    [SerializeField] Gradient m_ColorOverTemperature;
 
-    [FoldoutGroup("References"), SerializeField] Transform m_AttackPoint;
+    [SerializeField] Transform m_AttackPoint;
 
-    [FoldoutGroup("References"), SerializeField] GameObject m_MuzzleFlashPrefab;
-    [FoldoutGroup("References"), SerializeField] Bullet m_BulletPrefab;
+    [SerializeField] GameObject m_MuzzleFlashPrefab;
+    [SerializeField] Bullet m_BulletPrefab;
 
     [Space(10)]
-    [FoldoutGroup("References"), SerializeField] UnityEvent m_OnAttackFeedback;
-    [FoldoutGroup("References"), SerializeField] UnityEvent m_OnReloadFeedback;
+    [SerializeField] UnityEvent m_OnAttackFeedback;
+    [SerializeField] UnityEvent m_OnReloadFeedback;
 
     Material m_RendererMat;
 
@@ -59,6 +58,9 @@ public class RangeOverload_CombatStyle : CombatStyle
 
     private void OnEnable()
     {
+        m_HandleCoolsInput.action.Enable();
+        m_HandleCoolsSkillInput.action.Enable();
+
         m_HandleCoolsInput.action.started += Cool;
         m_HandleCoolsSkillInput.action.started += TryBuffOnCool;
     }
@@ -80,17 +82,17 @@ public class RangeOverload_CombatStyle : CombatStyle
     {
         switch (m_CurrentState)
         {
-            case WeaponState.DefaultCool:
+            case RangeOverloadWeaponState.DefaultCool:
                 HandleCool(m_DefaultCoolsPerSec);
                 break;
-            case WeaponState.OverloadCool:
+            case RangeOverloadWeaponState.OverloadCool:
                 HandleCool(m_DefaultCoolsPerSec);
                 break;
 
-            case WeaponState.CoolBuffed:
+            case RangeOverloadWeaponState.CoolBuffed:
                 HandleCool(m_BuffCoolsPerSec);
                 break;
-            case WeaponState.CoolNerfed:
+            case RangeOverloadWeaponState.CoolNerfed:
                 HandleCool(m_NerfCoolsPerSec);
                 break;
         }
@@ -103,14 +105,14 @@ public class RangeOverload_CombatStyle : CombatStyle
         if(m_CurentTemperature <= 0)
         {
             m_CurentTemperature = 0;
-            m_CurrentState = WeaponState.CanShoot;
+            m_CurrentState = RangeOverloadWeaponState.CanShoot;
         }
     }
 
     public override IEnumerator Attack()
     {
         if (m_CanAttack 
-            && (m_CurrentState == WeaponState.CanShoot || m_CurrentState == WeaponState.CoolBuffed))
+            && (m_CurrentState == RangeOverloadWeaponState.CanShoot || m_CurrentState == RangeOverloadWeaponState.CoolBuffed))
         {
             OnAttack?.Invoke();
             
@@ -138,39 +140,39 @@ public class RangeOverload_CombatStyle : CombatStyle
     void OnOverload()
     {
         m_CurentTemperature = 100;
-        m_CurrentState = WeaponState.Overload;
+        m_CurrentState = RangeOverloadWeaponState.Overload;
     }
 
     void Cool(InputAction.CallbackContext ctx)
     {
         switch (m_CurrentState)
         {
-            case WeaponState.CanShoot:
-                m_CurrentState = WeaponState.DefaultCool;
+            case RangeOverloadWeaponState.CanShoot:
+                m_CurrentState = RangeOverloadWeaponState.DefaultCool;
                 break;
-            case WeaponState.Overload:
-                m_CurrentState = WeaponState.OverloadCool;
+            case RangeOverloadWeaponState.Overload:
+                m_CurrentState = RangeOverloadWeaponState.OverloadCool;
                 break;
         }
     }
 
     void TryBuffOnCool(InputAction.CallbackContext ctx)
     {
-        if (m_CurrentState != WeaponState.OverloadCool) return;
+        if (m_CurrentState != RangeOverloadWeaponState.OverloadCool) return;
 
         if (m_CurentTemperature.InRange(m_RangeToBuff))
         {
-            m_CurrentState = WeaponState.CoolBuffed;
+            m_CurrentState = RangeOverloadWeaponState.CoolBuffed;
             m_CurentTemperature = 100;
         }
         else if (m_CurentTemperature.InRange(m_RangeToReset))
         {
-            m_CurrentState = WeaponState.CanShoot;
+            m_CurrentState = RangeOverloadWeaponState.CanShoot;
             m_CurentTemperature = 0;
         }
         else if (m_CurentTemperature.InRange(m_RangeToNerf))
         {
-            m_CurrentState = WeaponState.CoolNerfed;
+            m_CurrentState = RangeOverloadWeaponState.CoolNerfed;
             m_CurentTemperature = 100;
         }
     }
@@ -190,13 +192,13 @@ public class RangeOverload_CombatStyle : CombatStyle
 
     private void OnValidate()
     {
-        m_RangeToReset.x = Mathf.Clamp(m_RangeToReset.x, 0, m_RangeToReset.y);
-        m_RangeToReset.y = Mathf.Clamp(m_RangeToReset.y, m_RangeToReset.x, 100);
+        m_RangeToReset.x = Mathf.Clamp(m_RangeToReset.x, 0, 100);
+        m_RangeToReset.y = Mathf.Clamp(m_RangeToReset.y, 0, 100);
 
-        m_RangeToBuff.x = Mathf.Clamp(m_RangeToBuff.x, 0, m_RangeToBuff.y);
-        m_RangeToBuff.y = Mathf.Clamp(m_RangeToBuff.y, m_RangeToBuff.x, 100);
+        m_RangeToBuff.x = Mathf.Clamp(m_RangeToBuff.x, 0, 100);
+        m_RangeToBuff.y = Mathf.Clamp(m_RangeToBuff.y, 0, 100);
 
-        m_RangeToNerf.x = Mathf.Clamp(m_RangeToNerf.x, 0, m_RangeToNerf.y);
-        m_RangeToNerf.y = Mathf.Clamp(m_RangeToNerf.y, m_RangeToNerf.x, 100);
+        m_RangeToNerf.x = Mathf.Clamp(m_RangeToNerf.x, 0, 100);
+        m_RangeToNerf.y = Mathf.Clamp(m_RangeToNerf.y, 0, 100);
     }
 }
